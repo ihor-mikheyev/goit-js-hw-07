8c302d4a6ed60da72982b3a7f60358e7e2ba5675
Ihor Mikheyev <ihor.mikheiev@gmail.com>
2023-10-26 10:07:47 +0200

    ver2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function galleryMarkup(items) {
+    return items.map((item) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+           <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>`
+    ).join("");
+};
+
+const addGalleryMarkup = galleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", addGalleryMarkup);
+
+    var lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionsDelayTime: 250});
